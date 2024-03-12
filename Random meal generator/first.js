35c5c5e0addbb7ff36eba72262c5fbd6c5c8360f
Sachin Kumar <96949859+Sachin-Dashzer@users.noreply.github.com>
2024-03-12 19:59:26 +0000

    project completed

--- Random meal generator/first.js
+++ Random meal generator/first.js
@@ -3,36 +3,78 @@ const menu = document.querySelector(".menu");
 const mealbtn = document.querySelector("button");
 
 
+
+
+function itemslist(bata , items) {
+
+
+    for (let i = 1; i <= 20; i++) {
+
+		if (bata[`strIngredient${i}`]) {
+            items.push(`${bata[`strIngredient${i}`]} - ${bata[`strMeasure${i}`]} . `);
+
+        }
+        else {
+            break;
+        }
+
+    }
+
+
+}
+
+
+
+
+
 mealbtn.addEventListener("click", () => {
+
 
 
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
         .then((res) => res.json())
         .then((data) => {
 
-            console.log(data);
+            document.querySelector(".menu").style.display ="block";
 
+            let items = [];
+
+            itemslist(data?.meals[0] , items);
 
             menu.innerHTML = `
+                                <h1 class="title">${data?.meals[0]?.strMeal} <span>( ${data?.meals[0]?.strCategory} )</span></h1>
+                                <ul>
+                                    <li><span>Region : </span>${data?.meals[0]?.strArea}</li>
+                                    <li><span>Tags : </span>${data?.meals[0]?.strTags}</li>
+                                </ul>
 
-        <h1 class="title">${data?.meals[0]?.strMeal}</h1>
-        <ul>
-            <li><span>Region : </span>${data?.meals[0]?.strArea}</li>
-            <li><span>Categroy : </span>${data?.meals[0]?.strCategory}</li>
-            <li><span>Tags : </span>${data?.meals[0]?.strTags}</li>
-        </ul>
+                                <div class="ingredient">
+                                    <div class="img">
+                                        <img src="${data?.meals[0]?.strMealThumb}" alt=""> 
+                                    </div>
+                                    <div>
+                                        <h3>Required Ingredients</h3>
+                                        <ul>
+                                            ${items.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="recipe">
+                                    <h3>Recipe of ${data?.meals[0]?.strMeal} </h3>
+                                    <p>${data?.meals[0]?.strInstructions}</p>
+                                    
+                                    <h3>Video tutorial of ${data?.meals[0]?.strMeal} </h3>
 
-        <div class="ingredient">
-            <div class="img">
-                 <img src="${data?.meals[0]?.strMealThumb}" alt=""> 
-            </div>
-            <h5></h5>
-        </div>
-        <div class="recipe">
-        <p>${data?.meals[0]?.strInstructions}</p></div>
-`;
+                                    <iframe width="420" height="315"
+                                    src="https://www.youtube.com/embed/${data?.meals[0].strYoutube.slice(-11)}">
+                                    </iframe>
+                                    </div>
+                            `;
 
         })
+        .catch(
+            alert("Api not working due to some error")
+        );
 
 })
 
